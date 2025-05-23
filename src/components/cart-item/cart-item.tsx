b6f2c04cbaf7684a842895684de3da91e325f223
Okulov Dmitrii <okulovdmit@yandex.ru>
2025-05-23 22:33:29 +0500

    style(components/cart): added scroll for many items

--- src/components/cart-item/cart-item.tsx
+++ src/components/cart-item/cart-item.tsx
@@ -26,11 +26,15 @@ export const CartItem = ({
 			: 'Синий';
 	return (
 		<div className={styles.container}>
-			<img src={image} alt='' className={styles.img} />
-			<p>{name}</p>
-			<p>{namedColor}</p>
-			<p>{size}</p>
-			<p>{price}</p>
+			<img
+				src={image}
+				alt={`${name}-${namedColor}-${size}`}
+				className={styles.img}
+			/>
+			<p className={styles.name}>{name}</p>
+			<p className={styles.color}>{namedColor}</p>
+			<p className={styles.size}>{size}</p>
+			<p className={styles.price}>{price}</p>
 			<button type='button' onClick={() => deleteItem(id)}>
 				Удалить
 			</button>
